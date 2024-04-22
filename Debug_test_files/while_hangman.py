7a9752388bb7e930aa0ba4dbecc33a22b3bb232a
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2024-04-23 01:08:15 +0300

    add another file to debut hangman code

--- Debug_test_files/while_hangman.py
+++ Debug_test_files/while_hangman.py
@@ -16,18 +16,17 @@ for _ in range(word_length):
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
 
-goal = ""
-guess = input("Guess a letter: ").lower()
-while goal != chosen_word:
+
+while "_" in display:
+  guess = input("Guess a letter: ").lower()
 #Check guessed letter
   for position in range(word_length):
     letter = chosen_word[position]
 #      print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
     if letter == guess:
       display[position] = letter
-      goal += letter
-          
+  print(display)
+
+print("you did it")
     
 
-print(display)
-print(goal)
