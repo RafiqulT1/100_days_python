4b6270a0a560ce94f0ae7468605d841edc277dfe
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2024-05-13 22:16:33 +0300

    add OOP prctiece files and codes

---
+++ Practiece/day_16/main.py
@@ -0,0 +1,27 @@
+# https://pypi.org/project/prettytable/
+
+# from turtle import Turtle, Screen
+# # import another_module
+
+# # print(another_module.another_varibale)
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+print(table)
+
