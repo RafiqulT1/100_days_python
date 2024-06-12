0ef8f730c41d1590bb5d665fb6aa6bf884f67f6a
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2024-06-12 19:40:21 +0300

    reviese OOp

---
+++ Practiece/Revise_day_16_to_22/day_16/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import another_module
+
+print(another_module.another_variable)
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(10)
+
+my_screen = Screen()
+my_screen.exitonclick()
+
+
+
+
