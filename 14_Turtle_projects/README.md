dd1ad6f50cce7e2f8db4ed0207a259432d0ada60
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2024-06-24 22:41:46 +0300

    14_Turtle_projects: update README.md file, clean turtle_race.py code file

--- 14_Turtle_projects/README.md
+++ 14_Turtle_projects/README.md
@@ -10,10 +10,32 @@ This Python project uses the `turtle` module to create a colorful dot grid art i
 ### Installation
 Ensure that python is installed on your system.
 
-### Usage
-1. Download the `turtle_art.py´file.
+## Usage
+1. Download the `turtle_art.py` file.
 2. Run the script using Python
 ```
 python turtle_art.py
+```
+3. A window will open displaying the turtle graphics.
+
+## 2. Turtle Race
+This project is inspired by Horse race betting. Instead of horses we are going to put turtles on the line. 
+
+When `turtle_race.py` is executed, a pop-up window will appear asking the user to place a bet on turtle. User selects their lucky turtle from available color options. Once choice has been made the race begins as the turtles take random steps forward towards the finish line. The first turtle to reach the finish line is declared the winner.
+
+At the end of the race the result will be displayed in the terminal window, indicating whether the user's chosen turtle won or lost the race.
+
+### Requirements
+- Python 3.x
+- `turtle` module (typically included with Python's standard library package)
+
+### Installation
+Ensure that python is installed on your system.
+
+### Usage
+1. Download the `turtle_race.py´file.
+2. Run the script using Python
+```
+python turtle_race.py
 ```
 3. A window will open displaying the turtle graphics.
--- 14_Turtle_projects/turtle_race.py
+++ 14_Turtle_projects/turtle_race.py
@@ -1,37 +1,38 @@
 from turtle import Turtle, Screen
 import random
 
-is_race_on = False 
-screen = Screen() #create screen object from Screen() class 
+race_on = False 
+screen = Screen() #create screen object from Screen() class
 screen.setup(width=500, height=400) #set window size
 #ask for user input
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color (red / orange / yellow / green / blue / purple): ")
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"] # color list
 colored_turtles = [] #list to hold turtle objects
+
 change_y_axis = 0
 
-# loop to create turle objects based on color name
+# loop to create turtle objects based on color name
 for color in colors:
     turtle_name = color
-    print(turtle_name)
+    # print(turtle_name)
     turtle_name = Turtle(shape="turtle")
     turtle_name.color(color)
     turtle_name.penup()
     #place turtles side by side
-    turtle_name.goto(x=-230, y=-110 + change_y_axis) 
+    turtle_name.goto(x=-230, y=-110 + change_y_axis)
     colored_turtles.append(turtle_name)
     change_y_axis += 50
 
 #race starts if user has chosen the turtle color
 if user_bet:
-    is_race_on = True
+    race_on = True
 
 
-while is_race_on:
+while race_on:
     for turtle in colored_turtles:
         if turtle.xcor() > 215: #finnish line of the race
-            is_race_on = False 
+            race_on = False
             winner = turtle.pencolor()
             #let user know if their turle won / lost this race
             if winner == user_bet:
@@ -43,7 +44,7 @@ while is_race_on:
         random_distance = random.randint(0,10)
         turtle.forward(random_distance)
 
-# window close when clicked 
+# window close when clicked
 screen.exitonclick()
 
 
