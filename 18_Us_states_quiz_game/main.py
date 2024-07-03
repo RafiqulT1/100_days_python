ac7d7bce0feb5021e9fc59c4b303013ce6fa0157
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2024-07-03 21:32:36 +0300

    18_Us_states_quiz_game: add comments to the code files and rename variables

--- 18_Us_states_quiz_game/main.py
+++ 18_Us_states_quiz_game/main.py
@@ -1,12 +1,14 @@
-from read_csv import df, all_state_list
+# import calss and variables from files
+from read_csv import all_states
 from state_on_map import StatePlacement
+# import pandas and turtle
 import pandas
 import turtle
 
 
 # global variables
 guessesed_states = []
-total_states = len(all_state_list)
+total_states = len(all_states)
 total_guesses = len(guessesed_states)
 
 
@@ -19,25 +21,32 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-# create class
+# create object from StatePlacement class
 state_placement = StatePlacement()
 
 
-
+# main logic
 while total_guesses < total_states:
+    #set up prompt window
     user_guess = screen.textinput(title=f"{total_guesses}/{total_states} Correct Guesses",
     prompt="Please guess another state:").title()
 
-    for state in all_state_list:
+    # check user guess and place state name on map if the guess is correct
+    for state in all_states:
         if user_guess == state:
+            # append correctly guessed state to guessesed_states list and remove from all_state_list
             guessesed_states.append(user_guess)
-            all_state_list.remove(user_guess)
+            all_states.remove(user_guess)
+            # place state name on map
             state_placement.to_x_y_pos(user_guess)
 
+    # check if user wants to exit the game
     if user_guess == "Exit":
-        unguessed_states = all_state_list
+        unguessed_states = all_states
+        # create csv file of unguessed states
         unguessed_states_data = pandas.DataFrame(unguessed_states)
         unguessed_states_data.to_csv("unguessed_states.csv")
+        # create csv file of guessed states
         guessed_states_data = pandas.DataFrame(guessesed_states)
         guessed_states_data.to_csv("guessed_states.csv")
         break
--- 18_Us_states_quiz_game/state_on_map.py
+++ 18_Us_states_quiz_game/state_on_map.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 from read_csv import df
 
 class StatePlacement(Turtle):
+    "create turtle object for placing state name on map"
     def __init__(self):
         super().__init__()
         self.shape("turtle")
@@ -9,8 +10,7 @@ class StatePlacement(Turtle):
         self.penup()
 
     def to_x_y_pos(self, guessed):
+        "place guessed state on map according to x & y axis"
         guessed_state_data = df[df.state == guessed]
-        # print(guessed_state_data)
-        # print(f"{guessed_state_data.x}, {guessed_state_data.y}")
         self.goto(int(guessed_state_data.x), int(guessed_state_data.y))
         self.write(guessed)
