cd834e1fd29a9036d9ebf1928b9c4244bfc825a7
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2024-07-03 21:03:43 +0300

    18_Us_states_quiz_game: add freature of generating csv file of guessed & unguessed states. So that user can check what they were able to guess and which state they have missed

--- 18_Us_states_quiz_game/main.py
+++ 18_Us_states_quiz_game/main.py
@@ -1,13 +1,13 @@
 from read_csv import df, all_state_list
 from state_on_map import StatePlacement
+import pandas
 import turtle
 
 
 # global variables
+guessesed_states = []
 total_states = len(all_state_list)
-correct_guesses = 0
-
-
+total_guesses = len(guessesed_states)
 
 
 # set turtle window
@@ -24,16 +24,21 @@ state_placement = StatePlacement()
 
 
 
-game_on = True
-
-while correct_guesses < total_states:
-    user_guess = screen.textinput(title=f"{correct_guesses}/{total_states} Correct Guesses",
+while total_guesses < total_states:
+    user_guess = screen.textinput(title=f"{total_guesses}/{total_states} Correct Guesses",
     prompt="Please guess another state:").title()
 
     for state in all_state_list:
         if user_guess == state:
-            correct_guesses += 1
+            guessesed_states.append(user_guess)
+            all_state_list.remove(user_guess)
             state_placement.to_x_y_pos(user_guess)
 
+    if user_guess == "Exit":
+        unguessed_states = all_state_list
+        unguessed_states_data = pandas.DataFrame(unguessed_states)
+        unguessed_states_data.to_csv("unguessed_states.csv")
+        guessed_states_data = pandas.DataFrame(guessesed_states)
+        guessed_states_data.to_csv("guessed_states.csv")
+        break
 
-turtle.mainloop()
