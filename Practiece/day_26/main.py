96fc39b80b54f247c4caa6c4c47c8d4f68c99491
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2024-07-07 19:42:23 +0300

    Practiece/day_26: learning about dictionary comprihension

--- Practiece/day_26/main.py
+++ Practiece/day_26/main.py
@@ -1,3 +1,4 @@
+import random
 # name = "Rafiqul"
 # letter_list = [letter.lower() for letter in name]
 # print(letter_list)
@@ -6,13 +7,22 @@
 # num_list = [number*2 for number in numb_range]
 # print(num_list)
 
+# name_list = ["Alex", "Beth", "Cartoline", "Dave", "Elanor", "Freddie"]
+
+# short_names = [name for name in name_list if len(name) < 5]
+
+# long_names = [name.upper() for name in name_list if len(name) >= 5]
+
+# print(short_names)
+# print(long_names)
+
+# -------- DICTIONARY COMPREHENSION -------------
+
 name_list = ["Alex", "Beth", "Cartoline", "Dave", "Elanor", "Freddie"]
+students_scores = {student:random.randint(1,100) for student in name_list}
 
-short_names = [name
-               for name in name_list
-               if len(name) < 5]
+print(students_scores)
 
-long_names = [name.upper() for name in name_list if len(name) >= 5]
+passed_students = {student_name:score for (student_name, score) in students_scores.items() if score >= 50}
 
-print(short_names)
-print(long_names)
+print(passed_students)
