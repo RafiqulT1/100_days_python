d377b69443a0d4675f7a89271b572f3ca046e150
Rafiqul Talukder <rafiqul.t0071@gmail.com>
2024-07-14 01:15:30 +0300

    20_password_manager (GUI): add save password data to .txt file feature

--- 20_password_manager (GUI)/main.py
+++ 20_password_manager (GUI)/main.py
@@ -3,7 +3,26 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    # call get_info function to get website, email & password from entry
+    website, email_or_username, password = get_info() 
 
+    with open("saved_data.txt", mode="a") as password_file:
+        password_file.write(f"✤ {website} | {email_or_username} | {password}\n")
+
+    # call clear_info function to empty entries
+    clear_info()
+def get_info():
+    website = Website_entry.get()
+    email_or_username = Eml_username_entry.get()
+    password = password_entry.get()
+
+    return website, email_or_username, password
+
+def clear_info():
+    Website_entry.delete(0, "end")
+    Eml_username_entry.delete(0, "end")
+    password_entry.delete(0, "end")
 # ---------------------------- UI SETUP ------------------------------- #
 
 # Tkinter window and configuration
@@ -31,23 +50,24 @@ password_label.grid(column=0, row=3)
     #--------ENTRIES--------#
 # Website entry
 Website_entry = Entry(width=35)
-Website_entry.insert(END, string="enter website link") #Add some text to begin with
+# Website_entry.insert(END, string="enter website link") #Add some text to begin with
+Website_entry.focus() #auto set cursor to website entry box
 Website_entry.grid(column=1, row=1, columnspan=2)
 # Email/Username entry
 Eml_username_entry = Entry(width=35)
-Eml_username_entry.insert(END, string="enter username or email") #Add some text to begin with
+# Eml_username_entry.insert(END, string="enter username or email") #Add some text to begin with
 Eml_username_entry.grid(column=1, row=2, columnspan=2)
 # Password entry
-password_entry = Entry(width=21)
-password_entry.insert(END, string="enter password") #Add some text to begin with
+password_entry = Entry(width=20)
+# password_entry.insert(END, string="enter password") #Add some text to begin with
 password_entry.grid(column=1, row=3)
 
     #--------BUTTONS--------#
 # Generate Password button
-generate_psw_button = Button(text="Generate Password", width=10)
+generate_psw_button = Button(text="Generate Password", width=11)
 generate_psw_button.grid(column=2, row=3)
 # Add button
-add_button = Button(text="Add", width=33)
+add_button = Button(text="Add", width=33, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
